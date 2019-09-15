3e951219ba569cbb554dd3f68a954ef4a79ad38d
Hassan Bokhari <hassan.bokhari@nbc.ca>
2019-09-15 09:13:57 -0400

    implemented median

--- Backend/App.js
+++ Backend/App.js
@@ -3,9 +3,8 @@ var app = express();
 
 app.get('/primeNumber/:Num', function (req, res) {
    
-  var testing=parseInt(req.params.Num);
-  var results=primeNumbers((testing));
-  console.log(results);
+  var temp=parseInt(req.params.Num);
+  var results=primeNumbers(temp);
   res.send({ result: results });
 
 })
@@ -19,26 +18,45 @@ var server = app.listen(8081, function () {
 
 function primeNumbers(maxNumber){
 
-    const isPrime = new Array(maxNumber + 1).fill(true);
-    isPrime[0] = false;
-    isPrime[1] = false;
-  
-    const primes = [];
-  
-    for (let number = 2; number <= maxNumber; number += 1) {
-      if (isPrime[number] === true) {
-        primes.push(number);
-  
-        let nextNumber = number * number;
-  
-        while (nextNumber <= maxNumber) {
-          isPrime[nextNumber] = false;
-          nextNumber += number;
-        }
+  const isPrime = new Array(maxNumber + 1).fill(true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+
+  const primes = [];
+
+  for (let number = 2; number <= maxNumber; number += 1) {
+    if (isPrime[number] === true) {
+      primes.push(number);
+
+      let nextNumber = number * number;
+
+      while (nextNumber <= maxNumber) {
+        isPrime[nextNumber] = false;
+        nextNumber += number;
       }
     }
+  }
   
-    return primes;
+  return median(primes);
+}
+
+function median(numbers) {
+  var median = [], numsLen = numbers.length;
+
+  if (
+      numsLen % 2 === 0 // is even
+  ) {
+      // average of two middle numbers
+      var val1 = ([numsLen / 2 - 1]) ;
+      var val2=parseInt(val1)+1;
+      median=[numbers[val1],numbers[(val2)]];
+
+  } else { // is odd
+      // middle number only
+      var val=(numsLen - 1) / 2;
+      median = numbers[val];
   }
 
-  module.exports.primeNumbers=primeNumbers;
+  return median;
+}
+module.exports.primeNumbers=primeNumbers;
