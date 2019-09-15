5fa50616adf92efe02d493043b88d75a429a6ffd
Hassan Bokhari <hassan.bokhari@nbc.ca>
2019-09-14 20:48:45 -0400

    updated Title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,14 @@ import Form from './NameForm';
 
 
 export default class App extends Component {
+  componentDidMount() {
+    document.title = 'TouchBistro ReactApp';
+  }
   render() {
     return (
+      
     	<div>
-          <Form />
+         <Form />
     	</div>
     );
   }
