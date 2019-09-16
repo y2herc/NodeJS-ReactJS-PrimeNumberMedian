0e74c2eb07f7c980f761fffb631c845086072b74
Hassan Bokhari <hassan.bokhari@nbc.ca>
2019-09-15 21:20:55 -0400

    updated tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Form from './NameForm'
 
-it('App renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
-it('Form renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Form />, div);
+describe('Render component Test', () => {
+  it('App renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+  it('Form renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Form />, div);
+  });
 });
